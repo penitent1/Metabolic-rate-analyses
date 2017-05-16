ec468b47edb22d69cfef9db670e39485c5b402fc
Derek Somo <somo@zoology.ubc.ca>
2017-05-16 15:24:15 -0700

    - Added "trial number" and "pcrit-type" as data column headers
    - Streamlining R script for quick generation of SMR/"O2crit" numbers - record Pcrit's based on BOTH "raw" smr AND "best" SMR based on CV criterion specified by Chabot et al 2016, Fish Physiology paper on SMR
    
    - Added a csv with data for O2crit analysis of "big clgl", probe NFB0010, "Bottom clip"

--- SMR-Pcrit analysis generalized R script.R
+++ SMR-Pcrit analysis generalized R script.R
@@ -15,61 +15,37 @@ head(smr.data)
 
 probe09 <- smr.data[smr.data$probe == 'NFB0009', ] # Update as appropriate
 probe09.clgl <- probe09[probe09$spps == 'clgl', ] # Update as appropriate
-head(probe09.clgl) # Update as appropriate
-str(probe09.clgl) # Update as appropriate
-
-### Leaving off data recorded at less than 15 hrs in respirometry trial
-
-#probe09.15plus.clgl <- probe09.clgl[probe09.clgl$time.hrs > 15, ] # Update as appropriate
-#probe09.15plus.clgl # Update as appropriate
-#head(probe09.15plus.clgl) # Update as appropriate
-#str(probe09.15plus.clgl) # Update as appropriate
+probe09.clgl.trial1 <- probe09.clgl[probe09.clgl$trial.no == '1', ] # Update as appropriate
+head(probe09.clgl.trial1) # Update as appropriate
+str(probe09.clgl.trial1) # Update as appropriate
 
 ### SMR estimate function
 
-smr <- calcSMR(probe09.clgl$mo2) # Update as appropriate
+smr <- calcSMR(probe09.clgl.trial1$mo2) # Update as appropriate
 smr
 #> smr
 #$mlnd
 #1 
-#2.113142
+#2.273519
 
 ## $CVmlnd
 ## 1 
-## 9.648905
+## 7.251364
 smr.check.best <- as.numeric(ifelse(smr$CVmlnd > 5.4, smr$quant[4], smr$mlnd)) # as recommended in Chabot et al. 2016
 smr.check.best
 ## > smr.check.best
-## [1] 1.955556
+## [1] 2.142857
 
 ### Estimate Pcrit using the SMR obtained above
 
 pcrit.data <-read.csv(file.choose())
 head(pcrit.data)
 
-calcO2crit(pcrit.data, 1.96) # Enter value of SMR obtained above here, after "pcrit.data
+calcO2crit(pcrit.data, 2.14) # Enter value of SMR obtained above here, after "pcrit.data
 #?calcO2crit
 
-plotO2crit(calcO2crit(pcrit.data, 1.96))
-
-#> calcO2crit(pcrit.data, 2.20)
-#$o2crit
-#[1] 18.2 >> 18.2% Air Sat USING!! ALL data (not post-2sigma analysis)
-### In torr
-#> (18.2/100)*102.1*760*0.2095/101.325
-#[1] 29.19968
-
-#> calcO2crit(pcrit.data, 1.96)
-#$o2crit
-#[1] 18 >> 18% Air Sat USING!! ALL data (not post-2sigma analysis)
-### In torr
-#> (18/100)*102.1*760*0.2095/101.325
-#[1] 28.87881
-
-#> calcO2crit(pcrit.data, 2.20)
-#$o2crit
-#[1] 15.1 >> 15.1% Air sat using 2-sigma analysis data - no difference!
+plotO2crit(calcO2crit(pcrit.data, 2.14))
 
 ### In torr
-#> (15.1/100)*102.4*760*0.2095/101.325
-#[1] 24.29729
+#> (O2crit.%sat/100)*P.ATM.KPA*760*0.2095/101.325
+
