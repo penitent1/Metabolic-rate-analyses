9535d3b9a22361bdc3aa0188285eb3fea6a19060
Derek Somo <somo@zoology.ubc.ca>
2017-05-17 14:40:22 -0700

    - Added data files for pcrit analysis: clgl 8may2017

--- SMR-Pcrit analysis generalized R script.R
+++ SMR-Pcrit analysis generalized R script.R
@@ -13,15 +13,15 @@ head(smr.data)
 
 ### NFB00--: Specify probe and species for analysis
 
-probe09 <- smr.data[smr.data$probe == 'NFB0009', ] # Update as appropriate
-probe09.clgl <- probe09[probe09$spps == 'clgl', ] # Update as appropriate
-probe09.clgl.trial1 <- probe09.clgl[probe09.clgl$trial.no == '1', ] # Update as appropriate
-head(probe09.clgl.trial1) # Update as appropriate
-str(probe09.clgl.trial1) # Update as appropriate
+probe14 <- smr.data[smr.data$probe == 'NFB0014', ] # Update as appropriate
+probe14.clgl <- probe14[probe14$spps == 'clgl', ] # Update as appropriate
+probe14.clgl.trial2 <- probe14.clgl[probe14.clgl$trial.no == '2', ] # Update as appropriate
+head(probe14.clgl.trial2) # Update as appropriate
+str(probe14.clgl.trial2) # Update as appropriate
 
 ### SMR estimate function
 
-smr <- calcSMR(probe09.clgl.trial1$mo2) # Update as appropriate
+smr <- calcSMR(probe14.clgl.trial2$mo2) # Update as appropriate
 smr
 #> smr
 #$mlnd
@@ -41,10 +41,10 @@ smr.check.best
 pcrit.data <-read.csv(file.choose())
 head(pcrit.data)
 
-calcO2crit(pcrit.data, 3.27) # Enter value of SMR obtained above here, after "pcrit.data
+calcO2crit(pcrit.data, 2.13) # Enter value of SMR obtained above here, after "pcrit.data
 #?calcO2crit
 
-plotO2crit(calcO2crit(pcrit.data, 3.27))
+plotO2crit(calcO2crit(pcrit.data, 2.13))
 
 ### In torr
 #> (O2crit.%sat/100)*P.ATM.KPA*760*0.2095/101.325
