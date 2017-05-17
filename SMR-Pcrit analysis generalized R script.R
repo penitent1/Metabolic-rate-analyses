7c3d4f6d308f51dd8feb12b8eeb4dd3f208d5dd9
Derek Somo <somo@zoology.ubc.ca>
2017-05-17 09:25:38 -0700

    - changed a few object names to get data for a particular fish/trial

--- SMR-Pcrit analysis generalized R script.R
+++ SMR-Pcrit analysis generalized R script.R
@@ -13,20 +13,20 @@ head(smr.data)
 
 ### NFB00--: Specify probe and species for analysis
 
-probe09 <- smr.data[smr.data$probe == 'NFB0009', ] # Update as appropriate
-probe09.clgl <- probe09[probe09$spps == 'clgl', ] # Update as appropriate
-probe09.clgl.trial1 <- probe09.clgl[probe09.clgl$trial.no == '1', ] # Update as appropriate
-head(probe09.clgl.trial1) # Update as appropriate
-str(probe09.clgl.trial1) # Update as appropriate
+probe12 <- smr.data[smr.data$probe == 'NFB0012', ] # Update as appropriate
+probe12.olma <- probe12[probe12$spps == 'olma', ] # Update as appropriate
+probe12.olma.trial3 <- probe12.olma[probe12.olma$trial.no == '3', ] # Update as appropriate
+head(probe12.olma.trial3) # Update as appropriate
+str(probe12.olma.trial3) # Update as appropriate
 
 ### SMR estimate function
 
-smr <- calcSMR(probe09.clgl.trial1$mo2) # Update as appropriate
+smr <- calcSMR(probe12.olma.trial3$mo2) # Update as appropriate
 smr
 #> smr
 #$mlnd
 #1 
-#2.273519
+#1.330412
 
 ## $CVmlnd
 ## 1 
@@ -34,17 +34,17 @@ smr
 smr.check.best <- as.numeric(ifelse(smr$CVmlnd > 5.4, smr$quant[4], smr$mlnd)) # as recommended in Chabot et al. 2016
 smr.check.best
 ## > smr.check.best
-## [1] 2.142857
+## [1] 1.315862
 
 ### Estimate Pcrit using the SMR obtained above
 
 pcrit.data <-read.csv(file.choose())
 head(pcrit.data)
 
-calcO2crit(pcrit.data, 2.14) # Enter value of SMR obtained above here, after "pcrit.data
+calcO2crit(pcrit.data, 1.32) # Enter value of SMR obtained above here, after "pcrit.data
 #?calcO2crit
 
-plotO2crit(calcO2crit(pcrit.data, 2.14))
+plotO2crit(calcO2crit(pcrit.data, 1.32))
 
 ### In torr
 #> (O2crit.%sat/100)*P.ATM.KPA*760*0.2095/101.325
