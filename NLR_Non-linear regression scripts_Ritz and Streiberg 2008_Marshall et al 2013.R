8d7b36aff02572c2754d1c04a6f5527f731afa8c
Derek Somo <somo@zoology.ubc.ca>
2017-06-07 10:46:35 -0700

    - Rearranging unlabelled FOXY csv files into new folder - New script added for NON-LINEAR REGRESSION Practice

---
+++ NLR_Non-linear regression scripts_Ritz and Streiberg 2008_Marshall et al 2013.R
@@ -0,0 +1,5 @@
+?nls()
+install.packages("nlrwr", dependencies = TRUE)
+plot(num.fish ~ spawn.biomass,
+     data = M.merluccius, xlab = "Spawning biomass (1000 tonnes)",
+     ylab = "Recruitment (million fish)")
