55309dab596090cdc8815190e58f7254cb13df63
Derek Somo <somo@zoology.ubc.ca>
2017-06-10 13:48:16 -0700

    - Added 6 Jun 2017 OLMA 16 Deg C data to summary file

--- NLR_Non-linear regression scripts_Ritz and Streiberg 2008_Marshall et al 2013.R
+++ NLR_Non-linear regression scripts_Ritz and Streiberg 2008_Marshall et al 2013.R
@@ -3,3 +3,7 @@ install.packages("nlrwr", dependencies = TRUE)
 plot(num.fish ~ spawn.biomass,
      data = M.merluccius, xlab = "Spawning biomass (1000 tonnes)",
      ylab = "Recruitment (million fish)")
+
+
+
+
