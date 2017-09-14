4e99fd488fbad25f3d2ebd6fee8d6eefc1c309c4
Derek Somo <somo@zoology.ubc.ca>
2017-09-13 18:57:18 -0700

    - Data files for Pcrit-temp analyses

--- SMR-Pcrit analysis generalized R script.R
+++ SMR-Pcrit analysis generalized R script.R
@@ -20,16 +20,16 @@ head(smr.data)
 ### NFB00--: Specify probe and species for analysis
 
 probe13 <- smr.data[smr.data$probe == 'NFB0013', ] # Update as appropriate
-probe13.enbi <- probe13[probe13$spps == 'enbi', ] # Update as appropriate
-probe13.enbi.18aug <- probe13.enbi[probe13.enbi$date.day == '18' &
-                                     probe13.enbi$date.month == 'aug' , ] # Update as appropriate
+probe13.blci <- probe13[probe13$spps == 'blci', ] # Update as appropriate
+probe13.blci.1sep <- probe13.blci[probe13.blci$date.day == '1' &
+                                     probe13.blci$date.month == 'sep' , ] # Update as appropriate
 #probe14.olma.trial2.16c <- probe14.olma.trial2[probe14.olma.trial2$temp == '16', ] # Update as appropriate
-head(probe13.enbi.18aug) # Update as appropriate
-str(probe13.enbi.18aug) # Update as appropriate
+head(probe13.blci.1sep) # Update as appropriate
+str(probe13.blci.1sep) # Update as appropriate
 
 ### SMR estimate function
 
-five.hr.plus.data <- probe13.enbi.18aug[probe13.enbi.18aug$time.hrs > 5, ]
+five.hr.plus.data <- probe13.blci.1sep[probe13.blci.1sep$time.hrs > 5, ]
 
 smr <- calcSMR(five.hr.plus.data$mo2) # Update as appropriate
 smr
@@ -39,10 +39,10 @@ smr.check.best
 
   #############################
 
-plot.smr <- ggplot(data=probe13.enbi.18aug, aes(x=probe13.enbi.18aug$time.hrs, 
-                                                  y=probe13.enbi.18aug$mo2)) 
+plot.smr <- ggplot(data=probe13.blci.1sep, aes(x=probe13.blci.1sep$time.hrs, 
+                                                  y=probe13.blci.1sep$mo2)) 
 plot.smr + (geom_point(size = 1.5)) +
-  geom_hline(yintercept = 5.31) +
+  geom_hline(yintercept = 2.91) +
   labs(x = "Time (hrs)",
        y = "MO2 (umol O2/g/hr)") +
   theme_base()
@@ -67,10 +67,10 @@ plot.smr + (geom_point(size = 1.5)) +
 pcrit.data <-read.csv(file.choose())
 head(pcrit.data)
 
-calcO2crit(pcrit.data, 5.31) # Enter value of SMR obtained above here, after "pcrit.data
+calcO2crit(pcrit.data, 2.91) # Enter value of SMR obtained above here, after "pcrit.data
 #?calcO2crit
 
-plotO2crit(calcO2crit(pcrit.data, 5.31))
+plotO2crit(calcO2crit(pcrit.data, 2.91))
 
 ### In torr
 # (O2crit.%sat/100)*P.ATM.KPA*760*0.2095/101.325
