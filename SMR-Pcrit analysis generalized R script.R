24e46dceda619bad3e39b889c0ab4bdc10fc77a7
Derek Somo <somo@zoology.ubc.ca>
2017-10-07 16:10:20 -0700

    - Added data for pcrit analyses through 1 Oct 2017 OLMA at 20C

--- SMR-Pcrit analysis generalized R script.R
+++ SMR-Pcrit analysis generalized R script.R
@@ -15,34 +15,37 @@ library(fishMO2)
   # to get SMR and "O2crit" estimates
 
 smr.data <-read.csv(file.choose())
-head(smr.data)
+tail(smr.data)
 
 ### NFB00--: Specify probe and species for analysis
 
 probe13 <- smr.data[smr.data$probe == 'NFB0013', ] # Update as appropriate
-probe13.arha <- probe13[probe13$spps == 'arha', ] # Update as appropriate
-probe13.arha.7sep <- probe13.arha[probe13.arha$date.day == '7' &
-                                     probe13.arha$date.month == 'sep' , ] # Update as appropriate
+probe13.olma <- probe13[probe13$spps == 'olma', ] # Update as appropriate
+probe13.olma.01oct <- probe13.olma[probe13.olma$date.day == '1' &
+                                     probe13.olma$date.month == 'oct' , ] # Update as appropriate
 #probe14.olma.trial2.16c <- probe14.olma.trial2[probe14.olma.trial2$temp == '16', ] # Update as appropriate
-head(probe13.arha.7sep) # Update as appropriate
-str(probe13.arha.7sep) # Update as appropriate
+head(probe13.olma.01oct) # Update as appropriate
+str(probe13.olma.01oct) # Update as appropriate
 
 ### SMR estimate function
 
-five.hr.plus.data <- probe13.arha.7sep[probe13.arha.7sep$time.hrs > 5, ]
+five.hr.plus.data <- probe13.olma.01oct[probe13.olma.01oct$time.hrs > 5, ]
 
 smr <- calcSMR(five.hr.plus.data$mo2) # Update as appropriate
 smr
+
+#smr <- calcSMR(probe13.hehe.03aug$mo2) # Use if I need ALL the data eg ENBI!
+#smr
 
 smr.check.best <- as.numeric(ifelse(smr$CVmlnd > 5.4, smr$quant[4], smr$mlnd)) # as recommended in Chabot et al. 2016
 smr.check.best
 
   #############################
 
-plot.smr <- ggplot(data=probe13.arha.7sep, aes(x=probe13.arha.7sep$time.hrs, 
-                                                  y=probe13.arha.7sep$mo2)) 
+plot.smr <- ggplot(data=probe13.olma.01oct, aes(x=probe13.olma.01oct$time.hrs, 
+                                                  y=probe13.olma.01oct$mo2)) 
 plot.smr + (geom_point(size = 1.5)) +
-  geom_hline(yintercept = 3.28) +
+  geom_hline(yintercept = 2.20) +
   labs(x = "Time (hrs)",
        y = "MO2 (umol O2/g/hr)") +
   theme_base()
@@ -67,15 +70,33 @@ plot.smr + (geom_point(size = 1.5)) +
 pcrit.data <-read.csv(file.choose())
 head(pcrit.data)
 
-calcO2crit(pcrit.data, 3.28) # Enter value of SMR obtained above here, after "pcrit.data
+calcO2crit(pcrit.data, 2.20) # Enter value of SMR obtained above here, after "pcrit.data
 #?calcO2crit
 
-plotO2crit(calcO2crit(pcrit.data, 3.28))
+plotO2crit(calcO2crit(pcrit.data, 2.56))
 
 ### In torr
 # (O2crit.%sat/100)*P.ATM.KPA*760*0.2095/101.325
 
 
+
+
+####################################################
+####################################################
+####################################################
+####################################################
+####################################################
+####################################################
+####################################################
+####################################################
+####################################################
+####################################################
+####################################################
+####################################################
+####################################################
+####################################################
+####################################################
+####################################################
 
   #######################
   ## Subset and create datasets for comparing 16 hr vs "16+ hr" SMR estimates
