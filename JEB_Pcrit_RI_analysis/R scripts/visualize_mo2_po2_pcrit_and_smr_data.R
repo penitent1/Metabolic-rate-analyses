af28cd35ad5a7e14d8a3197bd1266957ee84e044
Derek Somo <somo@zoology.ubc.ca>
2018-11-16 16:00:32 -0800

    Added visualization script and figures to JEB-Pcrit-RI analysis

---
+++ JEB_Pcrit_RI_analysis/R scripts/visualize_mo2_po2_pcrit_and_smr_data.R
@@ -0,0 +1,34 @@
+## Load appropriate packages
+library(tidyverse)
+library(fishMO2)
+
+## Import data - someday I'll learn how to do this taking advantage of project format...
+# Alter as appropriate
+setwd("C:/Users/derek/Documents/Metabolic-rate-analyses/JEB_Pcrit_RI_analysis/Data")
+md_o2 <- read_csv("MO2_PO2_data_from_temp_vs_pcrit_study.csv")
+
+## Plot data: MO2 vs PO2 for SMR and Pcrit for each individual
+md_o2 %>%
+  mutate(mo2_raw_per_hr = umol_o2_per_sec*-1*3600,
+         mo2_ms_per_hr = mo2_raw_per_hr/mass_g) %>%
+  select(species_plotting,finclip_id,po2_torr,mo2_ms_per_hr, expt_period) %>%
+  dplyr::filter(expt_period == "pcrit") %>%
+  ggplot(aes(x = po2_torr, y = mo2_ms_per_hr)) +
+  facet_wrap(species_plotting ~ finclip_id) +
+  geom_point(size = 3) +
+  scale_y_continuous(name = expression(paste(dot(M),"o"[2]," (",mu,"mol ",O[2]," g"^-1," hr"^-1,")")),
+                     limits = c(0,8),
+                     breaks = seq(0,6,1)) +
+  scale_x_continuous(name = expression(paste("Po"[2]," (torr)")),
+                     limits = c(0,170),
+                     breaks = seq(0,160,10)) +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = NA),
+        axis.title.x = element_text(size = rel(5.5), margin = margin(t = 30)),
+        axis.title.y = element_text(size = rel(5.5), margin = margin(r = 30)),
+        strip.text = element_text(face = "bold", size = rel(1.5)),
+        axis.text.y = element_text(size = rel(1.5), colour = "black"),
+        axis.text.x = element_text(size = rel(1.25), colour = "black"),
+        axis.line = element_line(size = rel(1.5), colour = "black"),
+        axis.ticks = element_line(size = rel(5), colour = "black"))
