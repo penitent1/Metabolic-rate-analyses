9a2a923e80aec3f177376f0499bc9e4ecc9afe6e
Derek Somo <somo@zoology.ubc.ca>
2018-11-18 00:19:43 -0800

    Added data for MMR, SMR, and Pcrit for Ken's final tidepool sculpin from the MMR at Pcrit study - just need the MMR at Pcrit data for intop-bottom!

--- Ken Chu Pcrit project/KenChu_MMR-SMR-2day-Pcrit_fishMO2_analysis.R
+++ Ken Chu Pcrit project/KenChu_MMR-SMR-2day-Pcrit_fishMO2_analysis.R
@@ -43,10 +43,10 @@ md_lc <- md_lc %>%
 ## Take a look at the dataframe
 md_lc
 
-ggplot(md_lc$data[[16]], aes(x = time, y = mo2_raw/mass_g))+geom_point()+geom_line()
-ggplot(md_lc$data[[17]], aes(x = do_percent_sat, y = mo2_raw/mass_g))+geom_point(size = 3)
+ggplot(md_lc$data[[19]], aes(x = time, y = mo2_raw/mass_g))+geom_point()+geom_line()
+ggplot(md_lc$data[[20]], aes(x = do_percent_sat, y = mo2_raw/mass_g))+geom_point(size = 3)
 
-smr_data <- md_lc$data[[16]] %>% dplyr::select(do_percent_sat,po2_torr,mo2_raw,mass_g) %>% 
+smr_data <- md_lc$data[[19]] %>% dplyr::select(do_percent_sat,po2_torr,mo2_raw,mass_g) %>% 
   mutate(MO2 = mo2_raw/mass_g, DO = do_percent_sat) %>% as.data.frame(.)
 head(smr_data)
 
@@ -56,23 +56,23 @@ smr_est <- calcSMR(smr_data$MO2) # Top-bottom SMR = 1.13
 
 ifelse(smr_est$CVmlnd > 5.4, smr_est$quant, smr_est$mlnd)
 
-ggplot(md_lc$data[[16]], aes(x = time, y = mo2_raw/mass_g))+
+ggplot(md_lc$data[[19]], aes(x = time, y = mo2_raw/mass_g))+
   geom_point()+
   geom_line()+
-  geom_hline(yintercept = 1.33)
+  geom_hline(yintercept = 1.38)
 
-pcrit_data <- md_lc$data[[17]] %>% dplyr::select(do_percent_sat,po2_torr,mo2_raw,mass_g) %>% 
+pcrit_data <- md_lc$data[[20]] %>% dplyr::select(do_percent_sat,po2_torr,mo2_raw,mass_g) %>% 
   mutate(MO2 = mo2_raw/mass_g, DO = do_percent_sat) %>% as.data.frame(.)
 head(pcrit_data)
 
 #write_excel_csv(tibble(do = pcrit_data$DO, mo2 = pcrit_data$MO2),"C:/Users/derek/Documents/Metabolic-rate-analyses/Ken Chu Pcrit project/olma_pcrit_intop-inbottom.csv")
 
-plotO2crit(calcO2crit(pcrit_data, SMR = 1.14))
+plotO2crit(calcO2crit(pcrit_data, SMR = 1.38))
 
 smr_pcrit_data <- bind_rows(smr_data, pcrit_data)
 head(smr_pcrit_data)
 
-plotO2crit(calcO2crit(smr_pcrit_data, SMR = 1.33))
+plotO2crit(calcO2crit(smr_pcrit_data, SMR = 1.38))
 
 ## MMR at pcrit
 
@@ -102,17 +102,20 @@ mmr_at_pcrit_vs_time <- mmr_at_pcrit_vs_time %>% unnest() %>%
                             finclip_id == "top" ~ 1.42,
                             finclip_id == "top_bottom" ~ 1.13,
                             finclip_id == "intop_inbottom" ~ 1.85,
-                            finclip_id == "middle_stumpy" ~ 1.33),
+                            finclip_id == "middle_stumpy" ~ 1.33,
+                            finclip_id == "intop_bottom" ~ 1.38),
          mmr_ms = case_when(finclip_id == "bottom" ~ 8.82,
                             finclip_id == "top" ~ 7.75,
                             finclip_id == "top_bottom" ~ 6.83,
                             finclip_id == "intop_inbottom" ~ 8.71,
-                            finclip_id == "middle_stumpy" ~ 9.4),
+                            finclip_id == "middle_stumpy" ~ 9.40,
+                            finclip_id == "intop_bottom" ~ 6.34),
          fish_name = case_when(finclip_id == "bottom" ~ "Fish 1",
                                finclip_id == "top" ~ "Fish 2",
                                finclip_id == "top_bottom" ~ "Fish 3",
                                finclip_id == "intop_inbottom" ~ "Fish 4",
-                               finclip_id == "middle_stumpy" ~ "Fish 5"))
+                               finclip_id == "middle_stumpy" ~ "Fish 5",
+                               finclip_id == "intop_bottom" ~ "Fish 6"))
 
 # Plot: MO2 vs time, facet by individual
 ggplot(mmr_at_pcrit_vs_time) +
@@ -127,6 +130,51 @@ ggplot(mmr_at_pcrit_vs_time) +
   scale_y_continuous(name = expression(paste(dot(M),"o"[2]," (",mu,"mol O"[2]," g"^-1," h"^-1,")")),
                      limits = c(0,10),
                      breaks = seq(0,10,2)) +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_rect(fill = NA),
+        strip.text = element_text(size = rel(3), face = "bold"),
+        axis.title.x = element_text(size = rel(5.5), margin = margin(t = 30)),
+        axis.title.y = element_text(size = rel(5.5), margin = margin(r = 30)),
+        axis.text = element_text(size = rel(3), colour = "black"),
+        axis.line = element_line(size = rel(1.5), colour = "black"),
+        axis.ticks = element_line(size = rel(5), colour = "black"))
+## Print at W = 2000, H = 1239
+
+## Raw Pcrit data, faceted by individual
+raw_po2_vs_mo2 <- md_lc %>%
+  filter(expt_period == "pcrit") %>% 
+  unnest() %>%
+  mutate(mo2_ms = mo2_raw/mass_g,
+         smr_ms = case_when(finclip_id == "bottom" ~ 1.87,
+                            finclip_id == "top" ~ 1.42,
+                            finclip_id == "top_bottom" ~ 1.13,
+                            finclip_id == "intop_inbottom" ~ 1.85,
+                            finclip_id == "middle_stumpy" ~ 1.33,
+                            finclip_id == "intop_bottom" ~ 1.38),
+         mmr_ms = case_when(finclip_id == "bottom" ~ 8.82,
+                            finclip_id == "top" ~ 7.75,
+                            finclip_id == "top_bottom" ~ 6.83,
+                            finclip_id == "intop_inbottom" ~ 8.71,
+                            finclip_id == "middle_stumpy" ~ 9.40,
+                            finclip_id == "intop_bottom" ~ 6.34),
+         fish_name = case_when(finclip_id == "bottom" ~ "Fish 1",
+                               finclip_id == "top" ~ "Fish 2",
+                               finclip_id == "top_bottom" ~ "Fish 3",
+                               finclip_id == "intop_inbottom" ~ "Fish 4",
+                               finclip_id == "middle_stumpy" ~ "Fish 5",
+                               finclip_id == "intop_bottom" ~ "Fish 6"))
+
+
+ggplot(raw_po2_vs_mo2, aes(x = po2_torr, y = mo2_ms)) +
+  facet_wrap("fish_name") +
+  geom_point(size = 2) +
+  scale_x_continuous(name = expression(paste("Po"[2], " (Torr)")),
+                     limits = c(0,165),
+                     breaks = seq(0,160,20)) +
+  scale_y_continuous(name = expression(paste(dot(M),"o"[2]," (",mu,"mol O"[2]," g"^-1," h"^-1,")")),
+                     limits = c(0,5),
+                     breaks = seq(0,5,1)) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.background = element_rect(fill = NA),
