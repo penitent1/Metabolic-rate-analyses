32bf13b26517c1f31105f7fcbb0309c6872a53c0
Derek Somo <somo@zoology.ubc.ca>
2019-03-25 15:01:38 -0700

    Working on importing Pcrit data. Still.

--- Ken Chu Pcrit project/raw foxy csv files/MMR-2daySMR-Pcrit_MMRatPcrit/R scripts respR Analyses/BSR Pcrit analysis via respR.R
+++ Ken Chu Pcrit project/raw foxy csv files/MMR-2daySMR-Pcrit_MMRatPcrit/R scripts respR Analyses/BSR Pcrit analysis via respR.R
@@ -1,6 +1,14 @@
-getwd()
+library(tidyverse)
+library(respR)
+library(data.table)
+library(lubridate)
+
+## Define filepath to folder containing pcrit data files (.csv from NEOFOX software)
 data_path <- "C:/Users/derek/OneDrive/Documents/Metabolic-rate-analyses/Ken Chu Pcrit project/raw foxy csv files/MMR-2daySMR-Pcrit_MMRatPcrit/Raw foxy Pcrit files respR analyses"
+
+## Grab file names and add .csv to the end of each to generate a character vector of filenames
 files <- dir(data_path, pattern = "*.csv")
+
 ## Januar's function for parsing individual datafiles from NEOFOX (.csv files)
 parse_neofox <- function(x) {
   raw <- fread(x)
@@ -9,30 +17,37 @@ parse_neofox <- function(x) {
   out <- data.table("Elapsed (s)" = ntime, raw)
   return(out)
 }
+
+## Read data from "files" into a tibble
 pcrit_data <- tibble(filename = files) %>%
-  mutate(file_contents = filename %>% purrr::map(~ parse_neofox(file.path(data_path, .))),
-         inspect_pcrits = file_contents %>% purrr::map(~ inspect(.)))
+  mutate(file_contents = filename %>% purrr::map(~ parse_neofox(file.path(data_path, .)))) %>%
+  unnest() %>%
+  dplyr::select(filename, `Elapsed (s)`, Oxygen) %>%
+  group_by(filename) %>%
+  nest()
 
-pcrit_data$inspect_pcrits[[3]]
-  
+## Inspect data for each fish' pcrit trial and 
+pcrit_data$data[[1]]
+inspect(pcrit_data$data[[1]])
+pcrit(pcrit_data$data[[1]])
 
-library(data.table)
-library(lubridate)
+pcrit_data$data[[2]]
+inspect(pcrit_data$data[[2]])
+pcrit(pcrit_data$data[[2]])
 
-#parse_neofox <- function(path) {
-#  raw <- fread(path)
-#  dtime <- ymd_hms(raw$Time)
-#  ntime <- as.numeric(difftime(dtime, dtime[1], units = "secs"))
-#  out <- data.table("Elapsed (s)" = ntime, raw)
-#  return(out)
-#}
+pcrit_data$data[[3]]
+parse_neofox("C:/Users/derek/OneDrive/Documents/Metabolic-rate-analyses/Ken Chu Pcrit project/raw foxy csv files/MMR-2daySMR-Pcrit_MMRatPcrit/Raw foxy Pcrit files respR analyses/Ken_Tidepool_InTopInBottomFinClip_Pcrit_12C_NFB0008_2Nov2018.csv")
+inspect(pcrit_data$data[[3]])
+pcrit(pcrit_data$data[[3]])
 
-parse_neofox <- function(x) {
-  raw <- fread(x)
-  dtime <- ymd_hms(raw$Time)
-  ntime <- as.numeric(difftime(dtime, dtime[1], units = "secs"))
-  out <- data.table("Elapsed (s)" = ntime, raw)
-  return(out)
-}
+pcrit_data$data[[4]]
+inspect(pcrit_data$data[[4]])
+pcrit(pcrit_data$data[[4]])
 
-parse_neofox("./Ken_Tidepool_BottomFinClip_Pcrit_12C_NFB0013_05Oct2018.csv")
+pcrit_data$data[[5]]
+inspect(pcrit_data$data[[5]])
+pcrit(pcrit_data$data[[5]])
+
+pcrit_data$data[[6]]
+inspect(pcrit_data$data[[6]])
+pcrit(pcrit_data$data[[6]])
